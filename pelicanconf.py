b0035a8d6121a752442d996255fef95b66ec7499
valerio.versace <valerio.versace@soluzionifutura.it>
2016-02-06 00:19:47 +0100

    aggiunti contenuti di prova e impostazioni

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,6 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+STATIC_PATHS = ['extra/CNAME', '../README.md']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, '../README.md': {'path': 'README.md'}}
